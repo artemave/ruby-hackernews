7885228b247b5c868d53f52591732558b4d119f1
artemave <artemave@gmail.com>
2023-02-12 20:05:36 +0100

    Fix score always 0

--- lib/ruby-hackernews/services/parsers/voting_info_parser.rb
+++ lib/ruby-hackernews/services/parsers/voting_info_parser.rb
@@ -10,7 +10,7 @@ module RubyHackernews
     def parse
       upvote   = @voting_element[0]['href'] if @voting_element[0]
       downvote = @voting_element[1]['href'] if @voting_element[1]
-      points_element = @score_element.search("span")[0]
+      points_element = @score_element.search("[@class='score']")[0]
       score    = points_element.inner_html.split[0].to_i if points_element
       return VotingInfo.new(score, upvote, downvote)
     end
